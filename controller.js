11e1dceed19330cab98555d0975e279e348b8226
raleigh wayland <rtwayland@gmail.com>
2017-01-17 12:19:26 -0700

    complete

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles')
+    .controller('MainController', function($scope, mainService) {
+
+        $scope.getUsers = function() {
+            $scope.users = mainService.getUsers();
+        }
+
+        $scope.toggleFavorite = mainService.toggleFavorite;
+        
+        $scope.init = (function() {
+            $scope.getUsers();
+        })();
+    });
