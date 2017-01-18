0291820ab611afcaa70f39d90a61e365405b311a
raleigh wayland <rtwayland@gmail.com>
2017-01-18 12:02:31 -0700

    HTTP request. Disabled toggle function

--- mainService.js
+++ mainService.js
@@ -1,30 +1,16 @@
 angular.module('userProfiles')
-    .service('mainService', function() {
-        var data = [{
-                "id": 0,
-                "first_name": "george",
-                "last_name": "bluth",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-            },
-            {
-                "id": 1,
-                "first_name": "lucille",
-                "last_name": "bluth",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-            },
-            {
-                "id": 2,
-                "first_name": "oscar",
-                "last_name": "bluth",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-            }
-        ];
+    .service('mainService', function($http) {
 
-        this.getUsers = function () {
-          return data;
+
+        this.getUsers = function() {
+            return $http.get('https://reqres.in/api/users?page=1');
+            // return $http({
+            //   method: 'GET',
+            //   url: 'http://reqres.in/api/users?page=1'
+            // });
         }
 
         this.toggleFavorite = function (index) {
-          data[index].isFavorite = !data[index].isFavorite;
+          // data[index].isFavorite = !data[index].isFavorite;
         }
     });
